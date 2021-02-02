c68f6f9875c06a673c853e1da05d76e413a2d63a
ClementLiuKaYu <76940127+ClementLiuKaYu@users.noreply.github.com>
2021-02-02 15:54:02 +0800

    Update irrPoly drawLine

--- Project files/javascript/drawing-irr-polygon.js
+++ Project files/javascript/drawing-irr-polygon.js
@@ -4,78 +4,66 @@ class DrawingIrrPolygon extends PaintFunction {
   constructor(contextReal,contextDraft) {
     super();
     this.contextReal = contextReal;
-    this.contextDraft = contextDraft;           
-    this.end = true;
+    this.contextDraft = contextDraft;
+    this.start = false;
     this.coords = [];
   }
 
-  onMouseDown(coord, event){
-    if(this.end == true) {
-        this.coords = [];
-        this.origX = this.nextX = coord[0];
-        this.origY = this.nextY = coord[1];
-    }
-  }
-
-  onDragging(coord, event) {
+onMouseDown(coord, event){
+  if(this.start === false) {
+      this.origX = coord[0];
+      this.origY = coord[1];
+      this.start = true
+  }else if(Math.abs(coord[0] - this.origX) <  lastPointGap && Math.abs(coord[1] - this.origY) < lastPointGap && this.start == true) {
     this.contextDraft.clearRect(
       0,
       0,
       canvasDraft.width,
       canvasDraft.height
     );
-    this.draw(
-      this.nextX, 
-      this.nextY, 
-      coord[0], 
-      coord[1], 
-      this.contextDraft
-    );
-  }
 
-  onMouseMove(){}
-  
-  onMouseUp(coord, event) {    
+    this.contextReal.beginPath()
+    this.contextReal.moveTo(this.origX, this.origY);
+    for(let i = 0; i < this.coords.length; i++){
+      this.contextReal.lineTo(this.coords[i][0], this.coords[i][1]);
+    }
+    this.contextReal.closePath();
+    this.contextReal.stroke();
+    this.start = false;
+    this.coords = [];
+  }else{
+    this.coords.push([coord[0],coord[1]])
+  }
+}
+
+onDragging(coord, event) {
+
+}
+
+onMouseMove(coord, event){
+  if (this.start === true){
     this.contextDraft.clearRect(
       0,
       0,
       canvasDraft.width,
       canvasDraft.height
     );
-    this.draw(
-      this.nextX, 
-      this.nextY, 
-      coord[0], 
-      coord[1], 
-      this.contextReal
-    );
-    this.coords.push(coord);
-    this.nextX = coord[0];
-    this.nextY = coord[1]; 
-    //check the gap between last point and start point
-    if(Math.abs(this.nextX - this.origX) <  lastPointGap && Math.abs(this.nextY - this.origY) < lastPointGap && this.end == false) {
-      //connect last point and start point
-      let newPath = new Path2D();
-      newPath.moveTo(this.origX, this.origY);
-      for(let i = 0; i < this.coords.length; i++) {
-        newPath.lineTo(this.coords[i][0], this.coords[i][1]);
-      }
-      newPath.closePath();
-      this.contextReal.stroke(newPath);
-      this.end = true;
-      this.coords = [];
-    } else {
-      this.end = false;
+    this.contextDraft.beginPath()
+    this.contextDraft.moveTo(this.origX,this.origY)
+    for (let i=0; i < this.coords.length; i++){
+      this.contextDraft.lineTo(this.coords[i][0],this.coords[i][1])
     }
+    this.contextDraft.lineTo(coord[0],coord[1])
+    this.contextDraft.closePath()
+    this.contextDraft.stroke()
   }
+}
 
-  onMouseLeave(){}
-  onMouseEnter(){}
+onMouseUp(coord, event) {    
 
-  draw(x1, y1, x2, y2, context) {
-    context.beginPath();
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();    
-  }
+}
+
+onMouseLeave(){}
+onMouseEnter(){}
+
 }
